aab12df7f669a1d1f7c79c3d3acda40b057e1950
MariaMoor <68172786+MariaMoor@users.noreply.github.com>
2020-08-21 16:07:59 +0200

    Update run.py

--- app/run.py
+++ app/run.py
@@ -26,11 +26,11 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('DisasterResponse', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/classifier.pkl.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
@@ -39,6 +39,7 @@ model = joblib.load("../models/your_model_name.pkl")
 def index():
     
     # extract data needed for visuals
+    # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
     
@@ -50,7 +51,8 @@ def index():
     ct_n = j #names of the categories
     
     # create visuals
-        graphs = [
+    # TODO: Below is an example - modify to create your own visuals
+    graphs = [
         {
             'data': [
                 Bar(
